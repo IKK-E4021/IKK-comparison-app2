a50cdd9462fab1581a02db13a3ee3ed857d2343f
Shinta6348 <>
2021-01-24 19:03:00 +0900

    pry-byebug, annotate, erb2haml, rubocopなどのツール導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,25 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id                  :integer          not null, primary key
+#  contributor_comment :text
+#  item1               :string
+#  item2               :string
+#  item3               :string
+#  item4               :string
+#  item5               :string
+#  object_left         :string
+#  object_right        :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
 class Article < ApplicationRecord
-    validates :object_left, presence: true
-    validates :object_right, presence: true
-    validates :item1, presence: true
+  validates :object_left, presence: true
+  validates :object_right, presence: true
+  validates :item1, presence: true
 
-    def display_created_at
-        I18n.l(self.created_at, format: :default)
-    end
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id                  :integer          not null, primary key
+#  contributor_comment :text
+#  item1               :string
+#  item2               :string
+#  item3               :string
+#  item4               :string
+#  item5               :string
+#  object_left         :string
+#  object_right        :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
