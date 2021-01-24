c1b55fa88473adb0dd5302dc90a95b42cee5e953
Shinta6348 <>
2021-01-24 16:57:23 +0900

    日付 beforeアクション 部分テンプレート

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
     validates :object_left, presence: true
     validates :object_right, presence: true
     validates :item1, presence: true
+
+    def display_created_at
+        I18n.l(self.created_at, format: :default)
+    end
 end
